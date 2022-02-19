603e411cff2178648a421eeb3566d5730ae7b965
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2022-02-19 10:39:21 -0500

    update app commons

---
+++ django/ecommerce/commons/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+# Create your models here.
+
+# commons  -->
+# account --> DocumentType
+
+class Gender(models.Model):
+    short_name = models.CharField(max_length=5, )
+    long_name = models.CharField(max_length=100, )
+    is_active = models.BooleanField(default=True, )
+
+    def __str__(self):
+        return self.long_name
+
+
+class DocumentType(models.Model):
+    long_name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=55)
+    character_length = models.SmallIntegerField()
+    type_character = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.short_name
+
+# class Profile(models.Model):
+#     name= models.CharField(max_length=255, blank=False)
+#     document_type = models.ForeignKey(DocumentType, on_delete=models.CASCADE)
