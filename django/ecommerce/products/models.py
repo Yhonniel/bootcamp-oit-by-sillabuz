920e565e950190e8f9642f0db9d2f32baca642e6
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2022-02-18 22:06:52 -0500

    fix

--- django/ecommerce/products/models.py
+++ django/ecommerce/products/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Product(models.Model):
     sku = models.CharField(max_length=15, blank=False, unique=True)
     name = models.CharField(max_length=255, blank=False)
     stock = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
+    category = models.CharField(max_length=100, blank=False)
+    image = models.ImageField(
+        upload_to='static/img/product/',
+        default='static/img/product/1.png')
--- django/ecommerce/web/views.py
+++ django/ecommerce/web/views.py
@@ -4,9 +4,17 @@ from django.template import loader
 
 
 # Create your views here.
+from products.models import Product
+
+
 def index(request):
     context = {}
-    context['segment'] = 'index'
+    context['segment'] = 'index'  # Manegar navegacion
+
+    # Sentencia usando el ORM
+    products = Product.objects.filter()
+    print(products)
+    context['products'] = products
 
     html_template = loader.get_template('index.html')
     return HttpResponse(html_template.render(context, request))
